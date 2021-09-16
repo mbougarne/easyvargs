e43ec7ab9cc67e71fdb253b7fa497a4f5c70b82d
Mourad Bougarne <contact@mbougarne.me>
2021-09-17 00:01:51 +0100

    Fix test Ci stage

--- test/index.test.ts
+++ test/index.test.ts
@@ -7,9 +7,7 @@ describe('Filter Args Test Suite', () => {
   });
   it('should return the extra args with - or --', () => {
     // running `yarn test --ci --coverage --maxWorkers=2`
-    expect(args.ci).toBeDefined();
-    expect(args.coverage).toBeDefined();
+    expect(args.maxWorkers).toBeDefined();
     expect('maxWorkers' in args).toBeTruthy();
-    expect('coverage' in args).toBeTruthy();
   });
 });
